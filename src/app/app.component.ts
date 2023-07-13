ec42757d45457c47b981ffe092f0b028464765a8
saishreeya@gmail.com <skanta@ad.unc.edu>
2023-07-12 23:21:20 -0400

    working header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'personal-portfolio';
+  loadedFeature = 'home'|| 'contactMe' || 'courses' || 'resume' || 'projects';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
